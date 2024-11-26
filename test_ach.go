ba48ba1d594b7295648b06fc82f7d36986d6cf9f
Mark Boyd <markboyd42@googlemail.com>
2024-11-26 20:38:43 +0000

    test_ach, formatting.

--- test_ach.go
+++ test_ach.go
@@ -5,15 +5,15 @@ import "io/ioutil"
 import "os"
 import "strings"
 
-import	"gopkg.in/ini.v1"
+import "gopkg.in/ini.v1"
 
-import 	"privdump/achievements"
-import	"privdump/readers"
-import 	"privdump/types"
+import "privdump/achievements"
+import "privdump/readers"
+import "privdump/types"
 
 func read_file(filename string) (error, types.Header, []byte, map[int]*types.Form) {
 	h0 := types.Header{}
-	
+
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Println("Failed to load file", filename, "-", err)
@@ -32,78 +32,78 @@ func read_file(filename string) (error, types.Header, []byte, map[int]*types.For
 		}
 		forms[i] = &f
 	}
-	
+
 	return nil, header, bytes, forms
 }
 
 func main() {
 
 	cheev_map := map[string]func(types.Header, []byte, map[int]*types.Form) bool{}
-	for _, list := range(achievements.Cheev_list){
-		for _, cheev := range(list.Cheeves){
+	for _, list := range achievements.Cheev_list {
+		for _, cheev := range list.Cheeves {
 			cheev_map[cheev.Id] = cheev.Test
 		}
 	}
 
 	test_dir := "ach_test"
-	files, err := ini.Load(test_dir+"/files.ini")
-	if err !=nil{
+	files, err := ini.Load(test_dir + "/files.ini")
+	if err != nil {
 		fmt.Println("cant' even read ini file: %v", err)
 		os.Exit(1)
 	}
-	
+
 	error_count := 0
 
-	for _,s := range(files.Sections()){
+	for _, s := range files.Sections() {
 		if s.Name() != "DEFAULT" {
-			for _,file := range(strings.Split(s.Key("yes").String(), ",")){
-				filename := test_dir+"/"+strings.ToUpper(strings.TrimSpace(file))+".SAV"
-				
+			for _, file := range strings.Split(s.Key("yes").String(), ",") {
+				filename := test_dir + "/" + strings.ToUpper(strings.TrimSpace(file)) + ".SAV"
+
 				err, header, bytes, forms := read_file(filename)
-				
-				if err !=nil{
-					fmt.Println("While loading file:", filename, ":", err)
-					error_count+=1
+
+				if err != nil {
+					fmt.Println("While loading file:", filename, " for "+s.Name()+":", err)
+					error_count += 1
 					continue
 				}
-				
-				_,exists:= cheev_map[s.Name()]
-				if !exists{
+
+				_, exists := cheev_map[s.Name()]
+				if !exists {
 					fmt.Println("Error: achievemnt", s.Name(), "does not exist")
-					error_count+=1
+					error_count += 1
 					continue
 				}
-				
+
 				result := cheev_map[s.Name()](header, bytes, forms)
-				if !result{
+				if !result {
 					fmt.Println("File:", filename, "does not have achievement", s.Name())
-					error_count+=1
+					error_count += 1
 				}
 			}
-			for _,file := range(strings.Split(s.Key("no").String(), ",")){
-				filename := test_dir+"/"+strings.ToUpper(strings.TrimSpace(file))+".SAV"
-				
+			for _, file := range strings.Split(s.Key("no").String(), ",") {
+				filename := test_dir + "/" + strings.ToUpper(strings.TrimSpace(file)) + ".SAV"
+
 				err, header, bytes, forms := read_file(filename)
-				
-				if err !=nil{
-					fmt.Println("While loading file:", filename, ":", err)
-					error_count+=1
+
+				if err != nil {
+					fmt.Println("While loading file:", filename, " for "+s.Name()+":", err)
+					error_count += 1
 					continue
 				}
-				
-				_,exists:= cheev_map[s.Name()]
-				if !exists{
+
+				_, exists := cheev_map[s.Name()]
+				if !exists {
 					fmt.Println("Error: achievemnt", s.Name(), "does not exist")
-					error_count+=1
+					error_count += 1
 					continue
 				}
-				
+
 				result := cheev_map[s.Name()](header, bytes, forms)
-				if result{
+				if result {
 					fmt.Println("File:", filename, "has achievement", s.Name(), "and should not")
-					error_count+=1
+					error_count += 1
 				}
 			}
 		}
-	}	
+	}
 }
