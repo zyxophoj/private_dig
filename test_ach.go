51d2911e6f26c38207c371dba437e6cfc86b7991
Mark Boyd <markboyd42@googlemail.com>
2025-07-17 22:30:43 +0100

    test_ach, add testing for multi-file achievements

--- test_ach.go
+++ test_ach.go
@@ -11,6 +11,8 @@ import "privdump/achievements"
 import "privdump/readers"
 import "privdump/types"
 
+var test_dir = "ach_test"
+
 func read_file(filename string) (error, types.Header, []byte, map[int]*types.Form) {
 	h0 := types.Header{}
 
@@ -40,23 +42,46 @@ func boolmap[K any](t K, f K) map[bool]K {
 	return map[bool]K{true: t, false: f}
 }
 
+// real_filename constructs an actual filename from a string in the ini file.
+// These strings typically don't have extensions, so guess the extension based on
+// RF-ness.
+// However, if the string does have an extension, use it, and that can force
+// the achievement test to run in RF mode.
+func real_filename(file string, RF bool) (types.Game, string) {
+	game := types.GT_PRIV
+	ext := ".SAV"
+	if RF {
+		game = types.GT_RF
+		ext = ".PRS"
+	}
+	filename := test_dir + "/" + strings.ToUpper(strings.TrimSpace(file))
+	if !strings.Contains(file, ".") {
+		filename += ext
+	} else {
+		game = types.GT_RF
+		if RF && !strings.HasSuffix(filename, ".PRS") {
+			// This would be a bug in the ini file
+			fmt.Println("Error: Non-RF file read in RF mode")
+		}
+	}
+
+	return game, filename
+}
+
 func main() {
 
-	cheev_map := map[string]func(a *achievements.Arg) bool{}
+	cheev_map := map[string]achievements.Achievement{}
 	for _, list := range achievements.Cheev_list {
 		for _, cheev := range list.Cheeves {
-			cheev_map[cheev.Id] = cheev.Test
+			cheev_map[cheev.Id] = cheev
 		}
 	}
 	for _, list := range achievements.Cheev_list_rf {
 		for _, cheev := range list {
-			cheev_map[cheev.Id] = cheev.Test
+			cheev_map[cheev.Id] = cheev
 		}
 	}
 
-
-
-	test_dir := "ach_test"
 	files, err := ini.Load(test_dir + "/files.ini")
 	if err != nil {
 		fmt.Println("cant' even read ini file: %v", err)
@@ -67,41 +92,72 @@ func main() {
 
 	for _, s := range files.Sections() {
 		if s.Name() != "DEFAULT" {
-			game := types.GT_PRIV
-			ext := ".SAV"
-			if strings.HasPrefix(s.Name(), "AID_RF_"){
-				game=types.GT_RF
-				ext=".PRS"
+
+			cheev, exists := cheev_map[s.Name()]
+			if !exists {
+				fmt.Println("Error: achievment", s.Name(), "does not exist")
+				error_count += 1
+				continue
 			}
-			for _, expected := range []bool{true, false} {
-				for _, file := range strings.Split(s.Key(boolmap("yes", "no")[expected]).String(), ",") {
-					filename := test_dir + "/" + strings.ToUpper(strings.TrimSpace(file))
-					if !strings.Contains(file, "."){
-						filename += ext
-					} else {
-						game = types.GT_RF
+			is_rf := strings.HasPrefix(s.Name(), "AID_RF_")
+			if !cheev.Multi {
+				for _, expected := range []bool{true, false} {
+					for _, file := range strings.Split(s.Key(boolmap("yes", "no")[expected]).String(), ",") {
+
+						game, filename := real_filename(file, is_rf)
+						err, header, bytes, forms := read_file(filename)
+
+						if err != nil {
+							fmt.Println("While loading file:", filename, " for "+s.Name()+":", err)
+							error_count += 1
+							continue
+						}
+
+						if cheev.Test(&achievements.Arg{header, bytes, forms, game, nil, nil, ""}) != expected {
+							fmt.Printf(boolmap("File: %s does not have achievement %s\n", "File: %s has achievement %s but should not\n")[expected], filename, s.Name())
+							error_count += 1
+						}
+					}
+				}
+			} else {
+				files := strings.Split(s.Key("multi").String(), ",")
+
+				// The full list of files should get the achievement, but lists obtained by removing any one file should not.
+				file_lists := [][]string{files}
+				expected := []bool{true}
+				for i, _ := range files {
+					// This is necessary because append modifies its arguments... ouch!
+					files2 := append([]string{}, files...)
+
+					file_lists = append(file_lists, append(files2[:i], files2[i+1:]...))
+					expected = append(expected, false)
+				}
+
+				for i, list := range file_lists {
+					visited := map[uint8]bool{}
+					secrets := uint8(0)
+
+					result := false
+					for _, file := range list {
+						game, filename := real_filename(file, is_rf)
+						err, header, bytes, forms := read_file(filename)
+						if err != nil {
+							fmt.Println("While loading file:", filename, " for "+s.Name()+":", err)
+							error_count += 1
+							goto cheev_end
+						}
+						arg := achievements.Arg{header, bytes, forms, game, visited, &secrets, ""}
+						arg.Update()
+
+						result = cheev.Test(&arg)
 					}
 
-					err, header, bytes, forms := read_file(filename)
-
-					if err != nil {
-						fmt.Println("While loading file:", filename, " for "+s.Name()+":", err)
-						error_count += 1
-						continue
-					}
-
-					_, exists := cheev_map[s.Name()]
-					if !exists {
-						fmt.Println("Error: achievment", s.Name(), "does not exist")
-						error_count += 1
-						continue
-					}
-
-					if cheev_map[s.Name()](&achievements.Arg{header, bytes, forms, game, nil, nil, ""}) != expected {
-						fmt.Printf(boolmap("File: %s does not have achievement %s\n", "File: %s has achievement %s but should not\n")[expected], filename, s.Name())
+					if result != expected[i] {
+						fmt.Printf(boolmap("File list: %s does not have achievement %s\n", "File list: %s has achievement %s but should not\n")[expected[i]], list, s.Name())
 						error_count += 1
 					}
 				}
+			cheev_end:
 			}
 			delete(cheev_map, s.Name())
 		}
