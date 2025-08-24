b1e839f6db1711496a3bdbbab8fbe1fdcaaa14d3
Mark Boyd <markboyd42@googlemail.com>
2025-08-24 21:40:01 +0100

    Add privedit to make_release

--- make_release.bat
+++ make_release.bat
@@ -1,3 +1,4 @@
 go build priv_ach.go
+go build privedit.go
 del priv_ach.zip
-powershell Compress-Archive -Path priv_ach.exe,priv_ach.ini -DestinationPath priv_ach.zip
+powershell Compress-Archive -Path priv_ach.exe,privedit.exe,priv_ach.ini -DestinationPath priv_ach.zip
